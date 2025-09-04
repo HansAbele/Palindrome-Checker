618dd511f9dd2c754c6cf613d097dc1079368969
Johan Abel Taveras <1988jnts@gmail.com>
2025-09-03 21:48:33 -0400

    feat: add palindrome checker functionality

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("check-btn");
+const input = document.getElementById("text-input");
+const result = document.getElementById("result");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const text = input.value;
+  cleanedText = text.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const reversedText = cleanedText.split("").reverse().join("");
+  result.innerText =
+    cleanedText === reversedText
+      ? ` ${text} es palindromo`
+      : ` ${text} no es palindromo`;
+});
