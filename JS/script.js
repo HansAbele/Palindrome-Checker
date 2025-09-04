7b75b0f4264e4e12e2b9c7fe4563559e8f40e683
Johan Abel Taveras <1988jnts@gmail.com>
2025-09-03 21:53:46 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ const result = document.getElementById("result");
 button.addEventListener("click", (e) => {
   e.preventDefault();
   const text = input.value;
-  cleanedText = text.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const cleanedText = text.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   const reversedText = cleanedText.split("").reverse().join("");
   result.innerText =
     cleanedText === reversedText
