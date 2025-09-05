82371109ab97607ad5afd1e87d5a85367eee7809
Johan Abel Taveras <1988jnts@gmail.com>
2025-09-04 21:54:07 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,15 @@ const result = document.getElementById("result");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
+  if (input === "") {
+    alert("Please input a value")
+    return;
+  };
   const text = input.value;
   const cleanedText = text.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   const reversedText = cleanedText.split("").reverse().join("");
   result.innerText =
     cleanedText === reversedText
-      ? ` ${text} es palindromo`
-      : ` ${text} no es palindromo`;
+      ? ` ${text} is a palindrome`
+      : ` ${text} is not palindrome`;
 });
